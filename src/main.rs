b2bd54447c26269dad9b8a2a1b0c4246a888b199
WereDiggle <mingrui.roy.zou@gmail.com>
2020-04-28 21:13:55 -0400

    Add progress bar

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ fn main() {
 
     let mut image_buffer = DynamicImage::new_rgb8(image_width, image_height).to_rgb();
 
+    let mut progress_bar = progress::Bar::new();
+    progress_bar.set_job_title("Rendering...");
+
     for row in 0..image_height {
         for col in 0..image_width {
             let r: f64 = col as f64 / image_width as f64;
@@ -17,8 +20,10 @@ fn main() {
             let b = (255.999 * b).floor() as u8;
 
             image_buffer.put_pixel(col, image_height - 1 - row, Rgb([r,g,b]));
+            let progress = ((row*image_width + col)*100)/(image_height*image_width-1);
+            progress_bar.reach_percent(progress as i32);
         }
     }
 
-    image_buffer.save("test.png").unwrap();
+    image_buffer.save("output/test.png").unwrap();
 }
